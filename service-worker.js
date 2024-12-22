905164eae86e06aba841b75da3524aa66e2b8481
James Mudge <mudgej@outlook.com>
2024-12-22 00:32:20 +0000

    added new error checks and ability to use as PWA

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('nitrox-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png',
+        // Add any other files that should be cached
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cachedResponse) {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
